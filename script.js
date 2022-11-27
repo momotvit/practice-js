af555526bd426ac869fe02e50bceed675b16fddb
momotvit <110504519+momotvit@users.noreply.github.com>
2022-11-26 21:56:27 -0800

    new

--- script.js
+++ script.js
@@ -1,2 +1,114 @@
-console.log("hello world");
-console.table();
+
+//============ spread
+
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консоли будет массив
+// console.log(temps);
+// // ❌ Так не сработает, потому что передаём целый массив
+// console.log(Math.max(temps)); // NaN
+
+// // В консоли будет набор отдельных чисел
+// console.log(...temps);
+// // ✅ Распылим коллекцию элементов как отдельные аргументы
+// console.log(Math.max(...temps)); // 25
+
+
+
+// //===========rest
+
+
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значение первого аргумента
+//   console.log(secondNumber); // Значение второго аргумента
+//   console.log(otherArgs); // Массив остальных аргументов
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+
+///============================restructure
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: false,
+//   rating: 8.38,
+// };
+
+// const accessType = book.isPublic ? "публичном" : "закрытом";
+// console.log(accessType);
+// const message = `Книга ${book.title} автора ${book.author} с рейтингом ${book.rating} находится в ${accessType} доступе.`;
+
+
+// console.log(message);
+
+//////////////////////////////////========
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризируем
+// const { title, author, isPublic, rating, coverImage } = book;
+// console.log(coverImage); // undefined
+
+// const accessType = isPublic ? "публичном" : "закрытом";
+// const message = `Книга ${title} автора ${author} с рейтингом ${rating} находится в ${accessType} доступе.`;
+
+///////////////////////////////
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+// };
+
+// // Добавим картинку обложки если её нет в объекте книги
+// const {
+//   title,
+//   coverImage = "https://via.placeholder.com/640/480",
+//   author,
+// } = book;
+
+// console.log(title); // The Last Kingdom
+// console.log(author); // Bernard Cornwell
+// console.log(coverImage); // https://via.placeholder.com/640/
+
+
+
+
+   //////////////////////////
+
+// const rgb = [200, 255, 100];
+// const [red, green, blue] = rgb;
+
+// console.log(`R:${red},G:${green},B:${blue}`); // "R:200,G:255,B:100"
+
+
+//////////////////////////////////
+
+
+const rgb = [200, 100, 255];
+
+const [red, green, blue, alfa = [500]] = rgb;
+
+console.log(`R:${red},G:${green},B:${blue},Alfa:${alfa}`); // "R:200,G:100,B:255,Alfa:0.3"
+
+
+
+// //////////////////////////////////////////////////
+
+
+
+// const rgb = [200, 100, 255, 111, 333, 33334, 555];
+
+// const [, , , , ,blue] = rgb;
+
+// console.log(`Blue: ${blue}`); // "Blue: 255"
