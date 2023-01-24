d4230868a50bf92375fc8146b762201fe2531dce
momotvit <110504519+momotvit@users.noreply.github.com>
2023-01-23 23:12:31 -0800

    class

--- script.js
+++ script.js
@@ -4994,34 +4994,98 @@
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
 
-/////////////////////////////////////
+/////////////////////////////////////practice #3 have to come back to check
 
 
-const historyService = {
-  orders: [
-    { email: "jacob@hotmail.com", dish: "Burrito" },
-    { email: "solomon@topmail.net", dish: "Burger" },
-    { email: "artemis@coldmail.net", dish: "Pizza" },
-    { email: "solomon@topmail.net", dish: "Apple pie" },
-    { email: "jacob@hotmail.com", dish: "Taco" },
-  ],
-  // Change code below this line
-  getOrdersLog() {
-    return this.orders
-      .map(order => `email: ${order.email} dish: ${order.dish}`)
-      .join(" - ");
-  },
-  getEmails() {
-    const emails = this.orders.map(order => order.email);
-    const uniqueEmails = new Set(emails);
-    return [...uniqueEmails];
-  },
-  getOrdersByEmail(email) {
-    return this.orders.filter(order => order.email === email);
-  },
-  // Change code above this line
-};
-console.log(historyService.getOrdersLog())
-console.log(historyService.getEmails())
-console.log(historyService.getOrdersByEmail("solomon@topmail.net"))
-console.log(historyService.getOrdersByEmail("artemis@coldmail.net"))
+// const historyService = {
+//   orders: [
+//     { email: "jacob@hotmail.com", dish: "Burrito" },
+//     { email: "solomon@topmail.net", dish: "Burger" },
+//     { email: "artemis@coldmail.net", dish: "Pizza" },
+//     { email: "solomon@topmail.net", dish: "Apple pie" },
+//     { email: "jacob@hotmail.com", dish: "Taco" },
+//   ],
+//   // Change code below this line
+//   getOrdersLog() {
+//     return this.orders
+//       .map(order => `email: ${order.email} dish: ${order.dish}`)
+//       .join(" - ");
+//   },
+//   getEmails() {
+//     const emails = this.orders.map(order => order.email);
+//     const uniqueEmails = new Set(emails);
+//     return [...uniqueEmails];
+//   },
+//   getOrdersByEmail(email) {
+//     return this.orders.filter(order => order.email === email);
+//   },
+//   // Change code above this line
+// };
+// console.log(historyService.getOrdersLog())
+// console.log(historyService.getEmails())
+// console.log(historyService.getOrdersByEmail("solomon@topmail.net"))
+// console.log(historyService.getOrdersByEmail("artemis@coldmail.net"))
+
+
+
+///////////////////////////class,Object.keys,Object.create(obj),hasOwnProperty,
+
+// const animal = {
+//   eats: true,
+// };
+// const dog = Object.create(animal);
+// dog.barks = true;
+
+// const dogKeys = Object.keys(dog);
+
+// console.log(dogKeys); // ['barks']
+
+
+ ////////////////////////////
+ 
+ 
+//  class User {
+//   // Деструктуризируем объект
+//   constructor({ name, email }) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// const mango = new User({
+//   name: "Манго",
+//   email: "mango@mail.com",
+// });
+// console.log(mango); // { name: "Манго", email: "mango@mail.com" }
+
+// const poly = new User({
+//   name: "Поли",
+//   email: "poly@mail.com",
+// });
+// console.log(poly); // { name: "Поли", email: "poly@mail.com" }
+
+
+/////////////////////////////////
+
+
+class User {
+  static #takenEmails = [];
+  
+
+  static isEmailTaken(email) {
+    return this.#takenEmails.includes(email);
+  }
+
+  #email;
+
+  constructor({ email }) {
+    this.#email = email;
+    User.#takenEmails.push(email);
+    
+  }
+}
+
+const mango = new User({ email: "mango@mail.com" });
+
+console.log(User.isEmailTaken("poly@mail.com"));
+console.log(User.isEmailTaken("mango@mail.com"));
