3e30aa7b46e78444e001f87f628bfdf3ca1f7bd5
momotvit <110504519+momotvit@users.noreply.github.com>
2023-01-24 23:16:47 -0800

    new Set(array or element)

--- script.js
+++ script.js
@@ -4994,38 +4994,6 @@
 // console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
 
 
-/////////////////////////////////////practice #3 have to come back to check
-
-
-// const historyService = {
-//   orders: [
-//     { email: "jacob@hotmail.com", dish: "Burrito" },
-//     { email: "solomon@topmail.net", dish: "Burger" },
-//     { email: "artemis@coldmail.net", dish: "Pizza" },
-//     { email: "solomon@topmail.net", dish: "Apple pie" },
-//     { email: "jacob@hotmail.com", dish: "Taco" },
-//   ],
-//   // Change code below this line
-//   getOrdersLog() {
-//     return this.orders
-//       .map(order => `email: ${order.email} dish: ${order.dish}`)
-//       .join(" - ");
-//   },
-//   getEmails() {
-//     const emails = this.orders.map(order => order.email);
-//     const uniqueEmails = new Set(emails);
-//     return [...uniqueEmails];
-//   },
-//   getOrdersByEmail(email) {
-//     return this.orders.filter(order => order.email === email);
-//   },
-//   // Change code above this line
-// };
-// console.log(historyService.getOrdersLog())
-// console.log(historyService.getEmails())
-// console.log(historyService.getOrdersByEmail("solomon@topmail.net"))
-// console.log(historyService.getOrdersByEmail("artemis@coldmail.net"))
-
 
 
 ///////////////////////////class,Object.keys,Object.create(obj),hasOwnProperty,
@@ -5106,3 +5074,42 @@
 // objB.isOpen = false;
 // console.log(objB.x);
 // console.log(objA.isOpen)
+
+
+/////////////////////////////////////////
+
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map(order => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map(order => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter(order => order.email === email);
+  },
+  // Change code above this line
+};
+
+console.log(historyService.getOrdersLog());
+console.log(historyService.getEmails());
+console.log(historyService.getOrdersByEmail());
+
+
+
+const NewArray = [1, 1, 1, 1, 4, 5, 66, 4, 3, 6, 7, 7, 8];
+
+const uniqueNumbers = new Set(NewArray);
+console.log([...uniqueNumbers])
