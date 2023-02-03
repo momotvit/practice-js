0e2b52daaf9ce93edf174aace307edd5f3c9d1ca
momotvit <110504519+momotvit@users.noreply.github.com>
2023-02-03 15:01:00 -0800

    append, prepend, createElement, before, after, remove

--- script.js
+++ script.js
@@ -6047,13 +6047,62 @@
 
 
 
-const saveBtn = document.querySelector('.editor button[data-action="save"]');
-const closeBtn = document.querySelector('.editor button[data-action="close"]');
+// const saveBtn = document.querySelector('.editor button[data-action="save"]');
+// const closeBtn = document.querySelector('.editor button[data-action="close"]');
 
-console.log(saveBtn.dataset.action); //save
-console.log(closeBtn.dataset.action); //close
+// console.log(saveBtn.dataset.action); //save
+// console.log(closeBtn.dataset.action); //close
 
-const dishes = document.querySelectorAll(".dishes  > li");
-dishes.forEach((dish) => {
-  console.log(dish.dataset.id);
-});
+// const dishes = document.querySelectorAll(".dishes > li");
+// dishes.forEach((dish) => {
+//   console.log(dish.dataset.id);
+// });
+
+
+////////////////////////// create element
+
+
+// const heading = document.createElement("h1");
+// console.log(heading); // <h1></h1>
+
+// heading.textContent = "This is a heading";
+// console.log(heading); // <h1>This is a heading</h1>
+
+// const image = document.createElement("img");
+// image.src = "https://placeimg.com/640/480/nature";
+// image.alt = "Nature";
+// console.log(image); //
+
+
+/////////////////////////////////// append, prepend, createElement, before, after, remove
+
+
+
+// const list = document.querySelector(".usernames");
+
+// // Adds an item to the end of the list
+// const lastItem = document.createElement("li");
+// lastItem.textContent = "Poly";
+// list.append(lastItem);
+
+// // Adds an item to the beginning of the list
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Ajax";
+// list.prepend(firstItem);
+
+// // Adds a title before the list
+// const title = document.createElement("h2");
+// title.textContent = "USERNAMES";
+// title.style.textAlign = "center";
+// list.before(title);
+
+// // Adds a paragraph after the list
+// const text = document.createElement("p");
+// text.textContent =
+//   "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum tenetur assumenda fugiat maxime, soluta aspernatur quasi nihil in asperiores ad distinctio illo et debitis error iure voluptate numquam maiores nisi!";
+// list.after(text);
+
+
+// // removing element
+// const text1 = document.querySelector('.text1');
+// text1.remove();
